64bf9c3fbdef6f33697644a468487ab9e7b16ce9
abrahambtz <abrahambz@outlook.com>
2021-09-27 02:26:30 -0500

    Funcion de orden superior filter, map, reduce
    Funciones de orden superior son aquellas funciones que reciben como parametro funciones.

---
+++ 12-filter_map_reduce.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+def run():
+    # Funciones de orden superior son aqullas funciones que reciben como parametro funciones.
+    my_list = [1,2,3,4,5,6,7,8,9]
+    print(my_list)
+    odd = list(filter(lambda x: x%2 != 0, my_list))
+    squares = list(map(lambda x: x**2, my_list))
+    all_multiplied = reduce(lambda a, b: a*b, my_list)
+
+    print(odd)
+    print(squares)
+    print(all_multiplied)
+if __name__ == '__main__':
+    run()
